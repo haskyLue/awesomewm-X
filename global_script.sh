5f325a13e70b795e38439e5f2a369e93316414f6
pdq <idkbot@gmail.com>
2012-11-30 10:56:57 -0600

    updated awesomewm-X to current

--- global_script.sh
+++ global_script.sh
@@ -17,96 +17,31 @@ if [ -f "$tc1" ] && [ -f "$tc2" ]  ; then
 
 	nitrogen --restore &
 
-	if [ -z "$(pidof urxvtd)" ] ; then
-		urxvtd &
-	fi
-
-	if [ -z "$(pidof bitlbee)" ] ; then
-		sudo bitlbee -D
-	fi
-
-	if [ -z "$(pidof weechat-curses)" ] ; then
-		urxvtc -name IRC1 -e weechat-curses &
-		urxvtc -name IRC2 -e weechat-curses -d ~/.weechat-priv &
-	fi
-
-	if [ -z "$(pidof xbindkeys)" ] ; then
-		xbindkeys &
-	fi
-
-	if [ -z "$(pidof htop)" ] ; then
-		urxvtc -name Htop -e htop &
-	fi
-
-	if [ -z "$(pidof saidar)" ] ; then
-		urxvtc -name Saidar -e saidar -c &
-	fi
-
-	if [ -z "$(pidof ttyload)" ] ; then
-		urxvtc name TTYload -e ttyload &
-	fi
-
-	if [ -z "$(pidof journalctl)" ] ; then
-	    urxvtc -name Logs -e sudo journalctl -f &
-	fi
-
-	if [ -z "$(pidof htop)" ] ; then
-		urxvtc -name Htop -e htop &
-	fi
-
-	if [ -z "$(pidof vlc)" ] ; then
-	    vlc "$HOME/Videos/playlist" &
-	fi
-
-	if [ -z "$(pidof sublime_text)" ] ; then
-		subl &
-		sudo subl &
-	fi
-
-	if [ -z "$(pidof spacefm)" ] ; then
-		spacefm &
-	fi
-
-	if [ -z "$(pidof firefox)" ] ; then
-		firefox &
-	fi
-
-	if [ -z "$(pidof dropbox)" ] ; then
-		dropboxd &
-	fi
-
-	if [ -z "$(pidof parcellite)" ] ; then
-		parcellite &
-	fi
-
-	if [ -z "$(pidof transmission-daemon)" ] ; then
-		#transmission-qt > /dev/null 2>&1 &
-		sudo systemctl start transmission.service
-	fi
-
-	if [ -z "$(pidof mocp)" ] ; then
-		urxvtc -name MOCP -e mocp &
-	fi
-
-	if [ -z "$(pidof conky)" ] ; then
-		conky -d -c "$HOME"/.config/conky/.bottomrc &
-	else
-		killall conky
-		conky -d -c "$HOME"/.config/conky/.bottomrc &
-	fi
-
-	if [ -z "$(pidof httpd)" ] ; then
-		sudo lamp start
-	fi
-
-	if [ -z "$(pidof aarchup)" ] ; then
-		/usr/bin/aarchup --loop-time 30 --aur --icon pac_icon &
-	fi
-
+	[ -z "$(pidof urxvtd)" ] && urxvtd -q -o -f &
+	[ -z "$(pidof bitlbee)" ] && sudo bitlbee -D
+	[ -z "$(pidof weechat-curses)" ] && urxvtc -name IRC1 -e weechat-curses && urxvtc -name IRC2 -e weechat-curses -d ~/.weechat-priv &
+	[ -z "$(pidof xbindkeys)" ] && xbindkeys &
+	[ -z "$(pidof htop)" ] && urxvtc -name Htop -e htop &
+	[ -z "$(pidof saidar)" ] && urxvtc -name Saidar -e saidar -c &
+	[ -z "$(pidof ttyload)" ] && urxvtc name TTYload -e ttyload &
+	[ -z "$(pidof journalctl)" ] && urxvtc -name Logs -e sudo journalctl -f &
+	[ -z "$(pidof vlc)" ] && vlc "$HOME/Videos/playlist" &
+	[ -z "$(pidof sublime_text)" ] && subl && sudo subl &
+	[ -z "$(pidof spacefm)" ] && spacefm &
+	[ -z "$(pidof firefox)" ] && firefox &
+	[ -z "$(pidof dropbox)" ] && dropboxd &
+	[ -z "$(pidof urxvtd)" ] && parcellite &
+	[ -z "$(pidof transmission-daemon)" ] && sudo systemctl start transmission.service
+	[ -z "$(pidof mocp)" ] && urxvtc -name MOCP -e mocp &
+	[ -z "$(pidof conky)" ] && urxvtd &
+	[ -z "$(pidof conky)" ] && conky -d -c "$HOME"/.config/conky/.bottomrc &
+	[ -z "$(pidof httpd)" ] && sudo lamp start
+	[ -z "$(pidof aarchup)" ] && /usr/bin/aarchup --loop-time 30 --aur --icon pac_icon &
+	
 	# some leftover terminals
-	urxvtc -name Test &
-	urxvtc -name Play &
-	urxvtc -name STDerr -e tail -f "$HOME/.cache/awesome/stderr" &
+	#urxvtc -name Test &
+	#urxvtc -name Play &
+	#urxvtc -name STDerr -e tail -f "$HOME/.cache/awesome/stderr" &
 fi
 wmctrl -s 0
 exit 0
