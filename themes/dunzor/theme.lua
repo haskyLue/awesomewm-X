df4c9c1a5dd0e9d3cf1b6707eef5709a5fa5ff51
pdq <idkbot@gmail.com>
2012-12-16 11:37:12 -0600

    updated awesomewm-X to current working copy

--- themes/dunzor/theme.lua
+++ themes/dunzor/theme.lua
@@ -113,28 +113,28 @@ if home_path ~= nil then
 	-- }}}
 
 	-- {{{ Titlebar
-	theme.titlebar_close_button_focus  = themedir .. '/titlebar/close_focus.png'
-	theme.titlebar_close_button_normal = themedir .. '/titlebar/close_normal.png'
+	theme.titlebar_close_button_focus  = sharedthemes .. '/titlebar/close_focus.png'
+	theme.titlebar_close_button_normal = sharedthemes .. '/titlebar/close_normal.png'
 
-	theme.titlebar_ontop_button_focus_active    = themedir .. '/titlebar/ontop_focus_active.png'
-	theme.titlebar_ontop_button_normal_active   = themedir .. '/titlebar/ontop_normal_active.png'
-	theme.titlebar_ontop_button_focus_inactive  = themedir .. '/titlebar/ontop_focus_inactive.png'
-	theme.titlebar_ontop_button_normal_inactive = themedir .. '/titlebar/ontop_normal_inactive.png'
+	theme.titlebar_ontop_button_focus_active    = sharedthemes .. '/titlebar/ontop_focus_active.png'
+	theme.titlebar_ontop_button_normal_active   = sharedthemes .. '/titlebar/ontop_normal_active.png'
+	theme.titlebar_ontop_button_focus_inactive  = sharedthemes .. '/titlebar/ontop_focus_inactive.png'
+	theme.titlebar_ontop_button_normal_inactive = sharedthemes .. '/titlebar/ontop_normal_inactive.png'
 
-	theme.titlebar_sticky_button_focus_active    = themedir .. '/titlebar/sticky_focus_active.png'
-	theme.titlebar_sticky_button_normal_active   = themedir .. '/titlebar/sticky_normal_active.png'
-	theme.titlebar_sticky_button_focus_inactive  = themedir .. '/titlebar/sticky_focus_inactive.png'
-	theme.titlebar_sticky_button_normal_inactive = themedir .. '/titlebar/sticky_normal_inactive.png'
+	theme.titlebar_sticky_button_focus_active    = sharedthemes .. '/titlebar/sticky_focus_active.png'
+	theme.titlebar_sticky_button_normal_active   = sharedthemes .. '/titlebar/sticky_normal_active.png'
+	theme.titlebar_sticky_button_focus_inactive  = sharedthemes .. '/titlebar/sticky_focus_inactive.png'
+	theme.titlebar_sticky_button_normal_inactive = sharedthemes .. '/titlebar/sticky_normal_inactive.png'
 
-	theme.titlebar_floating_button_focus_active    = themedir .. '/titlebar/floating_focus_active.png'
-	theme.titlebar_floating_button_normal_active   = themedir .. '/titlebar/floating_normal_active.png'
-	theme.titlebar_floating_button_focus_inactive  = themedir .. '/titlebar/floating_focus_inactive.png'
-	theme.titlebar_floating_button_normal_inactive = themedir .. '/titlebar/floating_normal_inactive.png'
+	theme.titlebar_floating_button_focus_active    = sharedthemes .. '/titlebar/floating_focus_active.png'
+	theme.titlebar_floating_button_normal_active   = sharedthemes .. '/titlebar/floating_normal_active.png'
+	theme.titlebar_floating_button_focus_inactive  = sharedthemes .. '/titlebar/floating_focus_inactive.png'
+	theme.titlebar_floating_button_normal_inactive = sharedthemes .. '/titlebar/floating_normal_inactive.png'
 
-	theme.titlebar_maximized_button_focus_active    = themedir .. '/titlebar/maximized_focus_active.png'
-	theme.titlebar_maximized_button_normal_active   = themedir .. '/titlebar/maximized_normal_active.png'
-	theme.titlebar_maximized_button_focus_inactive  = themedir .. '/titlebar/maximized_focus_inactive.png'
-	theme.titlebar_maximized_button_normal_inactive = themedir .. '/titlebar/maximized_normal_inactive.png'
+	theme.titlebar_maximized_button_focus_active    = sharedthemes .. '/titlebar/maximized_focus_active.png'
+	theme.titlebar_maximized_button_normal_active   = sharedthemes .. '/titlebar/maximized_normal_active.png'
+	theme.titlebar_maximized_button_focus_inactive  = sharedthemes .. '/titlebar/maximized_focus_inactive.png'
+	theme.titlebar_maximized_button_normal_inactive = sharedthemes .. '/titlebar/maximized_normal_inactive.png'
 	-- }}}
 	theme.awesome_icon = home_path .. '.config/awesome/icons/menu_icon.png'
 
