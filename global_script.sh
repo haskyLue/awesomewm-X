5d1da96c1dcd150f7705a1a21bac7dbceda1c333
pdq <idkbot@gmail.com>
2012-12-17 03:40:56 -0600

    updated awesomewm-X to current working copy

--- global_script.sh
+++ global_script.sh
@@ -13,11 +13,10 @@
 tc1="/media/truecrypt1/test"
 tc2="/media/truecrypt2/test"
 
-## if not use truecrypt data comment following line
-if [ -f "$tc1" ] && [ -f "$tc2" ]  ; then
+if [ -f "$tc1" ] && [ -f "$tc2" ] || [ "$USER" != "pdq" ] ; then
 
 	nitrogen --restore &
-	[ -z "$(pidof compton)" ] && compton -cF
+	[ -z "$(pidof compton)" ] && compton -cF &
 	[ -z "$(pidof bitlbee)" ] && sudo bitlbee -D
 	[ -z "$(pidof weechat-curses)" ] && urxvtc -name IRC1 -e weechat-curses && urxvtc -name IRC2 -e weechat-curses -d ~/.weechat-priv
 	[ -z "$(pidof xbindkeys)" ] && xbindkeys &
@@ -25,7 +24,9 @@ if [ -f "$tc1" ] && [ -f "$tc2" ]  ; then
 	[ -z "$(pidof saidar)" ] && urxvtc -name Saidar -e saidar -c
 	[ -z "$(pidof tail)" ] && urxvtc -name STDerr -e tail -f "$HOME/.cache/awesome/stderr"
 	[ -z "$(pidof ttyload)" ] && urxvtc name TTYload -e ttyload
-	[ -z "$(pidof vlc)" ] && vlc "$HOME/Videos/playlist" &
+	if [ -d "$HOME/Videos/playlist" ] ; then
+		[ -z "$(pidof vlc)" ] && vlc "$HOME/Videos/playlist" &
+	fi
 	[ -z "$(pidof sublime_text)" ] && subl && sudo subl
 	#[ -z "$(pidof spacefm)" ] && spacefm &
 	#[ -z "$(pidof firefox)" ] && firefox &
@@ -33,11 +34,10 @@ if [ -f "$tc1" ] && [ -f "$tc2" ]  ; then
 	[ -z "$(pidof transmission-daemon)" ] && sudo systemctl start transmission.service
 	[ -z "$(pidof mocp)" ] && urxvtc -name MOCP -e mocp &
 	[ -z "$(pidof conky)" ] && conky -d -c "$HOME"/.config/conky/.bottomrc &
-	[ -z "$(pidof httpd)" ] && sudo lamp start
-	[ -z "$(pidof aarchup)" ] && /usr/bin/aarchup --loop-time 30 --aur --icon /home/pdq/.config/awesome/icons/pacman_icon_48x48.png &
+	[ -z "$(pidof httpd)" ] && sudo lamp.sh start
+	[ -z "$(pidof aarchup)" ] && /usr/bin/aarchup --loop-time 30 --aur --icon "$HOME/.config/awesome/icons/pacman_icon_48x48.png" &
 	sudo killall journalctl && urxvtc -name Logs -e sudo journalctl -f
 
-## if not use truecrypt data comment following line
 fi
 
 # focus tag 1
